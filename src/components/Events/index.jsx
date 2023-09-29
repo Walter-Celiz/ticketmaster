5d2e7da15446c2196b7fe4393ffa47a4e63bbc8f
Wolz <wceliz.dev@gmail.com>
2023-09-29 08:55:50 -0300

    feat: starting form

---
+++ src/components/Events/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import { EventItem } from "./components/EventItem";
+import data from "../../data/events.json";
+
+export const Events = ({ searchTerm }) => {
+  const {
+    _embedded: { events },
+  } = data;
+
+  const handleEventItemClick = (_id) => {
+    // console.log("evento clickeado: ", id);
+  };
+
+  const renderEvents = () => {
+    let eventsFiltered = events;
+
+    if (searchTerm.length > 0) {
+      eventsFiltered = eventsFiltered.filter((item) =>
+        item.name.toLocaleLowerCase().includes(searchTerm),
+      );
+    }
+
+    return eventsFiltered.map((eventItem) => (
+      <EventItem
+        key={eventItem.id}
+        name={eventItem.name}
+        info={eventItem.info}
+        image={eventItem.images[0].url}
+        onEventClick={handleEventItemClick}
+        id={eventItem.id}
+      />
+    ));
+  };
+
+  return (
+    <>
+      <p>Eventos</p>
+      {renderEvents()}
+    </>
+  );
+};
